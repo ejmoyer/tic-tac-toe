9e9067063263a275c5fedf1f561cb3931611b11e
Eric Moyer <ejmoyer3@comcast.net>
2019-11-25 15:51:14 -0500

    Made script work properly so it would display things inside the divs.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let grid = ['1', '2', '3', '4', '5', '6', '7', '8', '9'] ;
+let grid = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let turn = [0, 1];
 let players = ['X', 'O'];
 
 // as a player i want to see a grid so i can know the state of the game
 // make the grid and the divs reference one another
 for (let div in document.querySelectorAll("div")) {
-  document.querySelectorAll("div")[div].appendChild(document.createTextNode(grid[div]))
+  document.querySelectorAll('div')[div].appendChild(document.createTextNode(grid[div]))
 }
 //make sure the grid works
 
