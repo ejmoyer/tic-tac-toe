6389720c0da65711d69e83a138c32256c54a7ea6
Eric Moyer <ejmoyer3@comcast.net>
2019-11-25 14:46:21 -0500

    Initial commit.

---
+++ script.js
@@ -0,0 +1,17 @@
+// let grid = ;
+let turn = [0, 1];
+let players = ['X', 'O'];
+
+// as a player i want to see a grid so i can know the state of the game
+
+// as a player i want to click a square to place a marker
+
+// as a player i want to see who's turn it is to properly take turns
+
+// as a player i would like to have a button to reset the game
+// when someone wins
+
+// as a player i want to see a result so i can know the outcome
+
+// as a player i want new games to be randomized who starts
+// so that its fair
