9ac907985794bd0032b28714be39a0b83bd5a11f
Eric Moyer <ejmoyer3@comcast.net>
2019-11-25 15:33:10 -0500

    Added loop to read the divs and the grid arrays.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-// let grid = ;
+let grid = ['1', '2', '3', '4', '5', '6', '7', '8', '9'] ;
 let turn = [0, 1];
 let players = ['X', 'O'];
 
 // as a player i want to see a grid so i can know the state of the game
+// make the grid and the divs reference one another
+for (let div in document.querySelectorAll("div")) {
+  document.querySelectorAll("div")[div].appendChild(document.createTextNode(grid[div]))
+}
+//make sure the grid works
 
 // as a player i want to click a square to place a marker
-
+// add an event listener for click
+// it adds a text node to the div
 // as a player i want to see who's turn it is to properly take turns
 
 // as a player i would like to have a button to reset the game
