c129aabce4b5b9cb000603ce10e76d84e7261ad0
Eric Moyer <ejmoyer3@comcast.net>
2019-11-26 12:27:41 -0500

    It now alternates X and Os, put it into a function and made it remove the event listener after being clicked once.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  
+
 let grid = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let turn = [0, 1];
 const players = ['X', 'O'];
@@ -7,19 +7,20 @@ let divs = Array.from(document.querySelectorAll("div"))
 let counter = 0;
 for (let div in divs) {
   divs[div].appendChild(document.createTextNode(""))
-  divs[div].addEventListener("click", event => {
+  const symbolFunc = event => {
     counter++;
-    if (counter / 2 == 0) {
+    if (counter % 2 == 0) {
         divs[div].replaceChild(document.createTextNode(players[0]), divs[div].firstChild);
 }
     else {
-              divs[div].replaceChild(document.createTextNode(players[1]), divs[div].firstChild);
+        divs[div].replaceChild(document.createTextNode(players[1]), divs[div].firstChild);
         }
+        divs[div].removeEventListener("click", symbolFunc);
+}
+  divs[div].addEventListener("click", symbolFunc);
+}
 
-})
-}
-removeEventListener(event);
-}
+
 //make sure the grid works
 
 // as a player i want to click a square to place a marker
