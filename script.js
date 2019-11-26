94410ad9d351d7b7e356a617243899b3af43986b
Eric Moyer <ejmoyer3@comcast.net>
2019-11-26 13:54:40 -0500

    Made the starting player random each time, replaced p tag default value with a space.

--- script.js
+++ script.js
@@ -3,25 +3,42 @@ let turn = [0, 1];
 const players = ['X', 'O'];
 
 let divs = Array.from(document.querySelectorAll("div"))
-let counter = 0;
+function randomPlayer(playerNumber) {
+  let randomNumber = Math.random();
+  if (randomNumber >= .5) {
+  return Math.ceil(randomNumber * Math.floor(playerNumber));
+} else {
+  return Math.floor(randomNumber * Math.floor(playerNumber));
+}
+}
+
+let counter = randomPlayer(1);
+window.addEventListener("load", event => {
+  if (counter % 2 != 0){
+  document.querySelector('p').replaceChild(document.createTextNode(`Player 2's Turn`), document.querySelector('p').firstChild)
+} else {
+    document.querySelector('p').replaceChild(document.createTextNode(`Player 1's Turn`), document.querySelector('p').firstChild)
+}
+});
+
 for (let div in divs) {
   divs[div].appendChild(document.createTextNode(""))
   const symbolFunc = event => {
-    counter++;
     if (counter % 2 == 0) {
       divs[div].replaceChild(document.createTextNode(players[0]), divs[div].firstChild);
     } else {
       divs[div].replaceChild(document.createTextNode(players[1]), divs[div].firstChild);
     }
+    counter++;
     divs[div].removeEventListener("click", symbolFunc);
   }
-  divs[div].addEventListener("click", symbolFunc);
 
   divs[div].addEventListener("click", event => {
     if (counter % 2 != 0) {
-      document.querySelector("p").replaceChild(document.createTextNode(`Player 2's Turn`), document.querySelector('p').firstChild)
-  } else {
       document.querySelector("p").replaceChild(document.createTextNode(`Player 1's Turn`), document.querySelector('p').firstChild)
+  } else {
+      document.querySelector("p").replaceChild(document.createTextNode(`Player 2's Turn`), document.querySelector('p').firstChild)
   }
   })
+  divs[div].addEventListener("click", symbolFunc);
 }
