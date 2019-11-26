5dc192ec6e9743b01a3c9500a9c6d81f6d631e4f
Eric Moyer <ejmoyer3@comcast.net>
2019-11-26 12:39:43 -0500

    Removed useless comments

--- script.js
+++ script.js
@@ -19,21 +19,3 @@ for (let div in divs) {
 }
   divs[div].addEventListener("click", symbolFunc);
 }
-
-
-//make sure the grid works
-
-// as a player i want to click a square to place a marker
-
-
-// add an event listener for click
-// it adds a text node to the div
-// as a player i want to see who's turn it is to properly take turns
-
-// as a player i would like to have a button to reset the game
-// when someone wins
-
-// as a player i want to see a result so i can know the outcome
-
-// as a player i want new games to be randomized who starts
-// so that its fair
